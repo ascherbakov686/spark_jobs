156feb7a7f35f5a3f35d84bf5ac49687f7d63942
Scherbakov Alexey <ascherbakov-ya@yandex.ru>
2021-05-27 10:06:53 +0300

    Create impalaQueryHistoryCrawler_run_v2.sh

---
+++ impalaQueryHistoryCrawler_run_v2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+job_home="/root/impalaQueryHistoryCrawler/"
+
+keytab_path=$(ls -1trd /var/run/cloudera-scm-agent/process/*impala-IMPALAD |tail -1);
+
+cd $job_home;
+
+exec >>impalaQueryHistoryCrawler_v2.log 2>&1
+
+kinit impala/node10.avp.ru@DOMAIN.RU -k -t $keytab_path/impala.keytab;
+
+#spark2-submit --queue root.impala --driver-memory 2g --executor-memory 2g --master yarn  --deploy-mode cluster  impalaQueryHistoryCrawler.py
+spark2-submit  --queue root.impala --conf spark.network.timeout=800 --driver-memory 2g --executor-memory 2g  --master yarn  --deploy-mode client  impalaQueryHistoryCrawler_v2.py
+
+impala-shell --ssl -k --quiet -B -d impala_queries -q "refresh history_profile"
