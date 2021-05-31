61f317bb9a3f3c8e44e479d30e6c011b250e7893
Scherbakov Alexey <ascherbakov-ya@yandex.ru>
2021-05-31 11:06:57 +0300

    Update impalaQueryHistoryCrawler_run_v2.sh

--- impalaQueryHistoryCrawler_run_v2.sh
+++ impalaQueryHistoryCrawler_run_v2.sh
@@ -11,6 +11,6 @@ exec >>impalaQueryHistoryCrawler_v2.log 2>&1
 kinit impala/node10.avp.ru@DOMAIN.RU -k -t $keytab_path/impala.keytab;
 
 #spark2-submit --queue root.impala --driver-memory 2g --executor-memory 2g --master yarn  --deploy-mode cluster  impalaQueryHistoryCrawler.py
-spark2-submit  --queue root.impala --conf spark.network.timeout=800 --driver-memory 2g --executor-memory 2g  --master yarn  --deploy-mode client  impalaQueryHistoryCrawler_v2.py
+spark2-submit  --queue root.impala --conf spark.network.timeout=800 --driver-memory 2g --executor-memory 2g  --master yarn  --deploy-mode client --py-files durationpy.zip impalaQueryHistoryCrawler_v2.py
 
 impala-shell --ssl -k --quiet -B -d impala_queries -q "refresh history_profile"
